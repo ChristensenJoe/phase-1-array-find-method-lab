e83f191dfa183c31af0dfbe530e7a317a68f883b
Joe Christensen <joechristensenwork@gmail.com>
2021-06-28 16:15:06 -0700

    commit tree dbaad7adbdea54e5bc88bb1027dda9b9564ecadc

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+const superbowlWin = (arr) => {
+    let obj = arr.find((element) => {
+        return element.result === "W";
+    });
+    if(obj != undefined) {
+        return obj.year;
+    }
+    return obj;
+}
